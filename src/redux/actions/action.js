059640c3a1da47bccad2ed1e1b31d9a17a5b4e75
sukrithi s <sukriti@stellacenter.dev>
2021-10-25 12:14:22 +0530

    added array_list in action type to commited main branch

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -21,4 +21,5 @@ export const buyBook = () => {
 
 
 
+
 // export default buyLaptop
--- src/redux/actions/actionType.js
+++ src/redux/actions/actionType.js
@@ -1,3 +1,4 @@
 export const BUY_LAPTOP = "BUY_LAPTOP"
 export const  BUY_MOBILE= "BUY_MOBILE"
 export const  BUY_BOOK= "BUY_BOOk"
+export const ARRAY_LIST = "ARRAY_LIST"
